38286a720d4a1fdc048f26e1c0cb40b1415abc57
hery2727 <48321246+hery2727@users.noreply.github.com>
2020-05-22 22:15:53 +0700

    update dari github editor

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -6,6 +6,6 @@ func main() {
 	var input string
 	fmt.Println("Masukkan Nama Anda: ")
 	i, r := fmt.Scanln(&input)
-	fmt.Printf("Hasil Scanln: length = %v, error = %v", i, r)
+	fmt.Printf("Hasil Scanln: jumlah input = %v, error = %v", i, r)
 	fmt.Printf("input: %v", input)
 }
