fbf4a5f8b1ff69b4236bce5b3413757e98fc2efe
Hery Andrian <heryandrian.engineer@gmail.com>
2020-05-22 21:53:18 +0700

    fix typo

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -6,6 +6,6 @@ func main() {
 	var input *int
 	fmt.Println("Masukkan Nama Anda: ")
 	i, r := fmt.Scanln(input)
-	fmt.Printf("Hasil ScanLn: length = %v, error = %v", i, r)
+	fmt.Printf("Hasil Scanln: length = %v, error = %v", i, r)
 	fmt.Printf("input: %v", input)
 }
