01101860e5ebdfa7696ed0c1a62bcd2dbb88eea7
Hery Andrian <heryandrian.engineer@gmail.com>
2020-05-22 22:06:40 +0700

    fix input to string

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var input *int
+	var input string
 	fmt.Println("Masukkan Nama Anda: ")
-	i, r := fmt.Scanln(input)
+	i, r := fmt.Scanln(&input)
 	fmt.Printf("Hasil Scanln: length = %v, error = %v", i, r)
 	fmt.Printf("input: %v", input)
 }
